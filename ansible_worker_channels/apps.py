3960d7730a6aabadecb6edc47b0976b45a649b3f
Ben Thomasson <bthomass@redhat.com>
2018-11-08 10:35:57 -0500

    Cleanup

--- ansible_worker_channels/apps.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8
-from django.apps import AppConfig
-
-
-class AnsibleWorkerChannelsConfig(AppConfig):
-    name = 'ansible_worker_channels'
--- ansible_worker_channels/routing.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import consumers
-
-channel_patterns = {
-    "hello": consumers.HelloConsumer,
-    "ansible": consumers.AnsibleConsumer,
-}
